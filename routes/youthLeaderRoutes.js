3a5cb1ddf27a8212e6d6467d5bbcca8075b2992a
Eric RWIGEMA <erwigema@bk.rw>
2025-05-29 11:22:34 +0200

    feat: unify youth leaders into /api/leaders module and remove redundant routes

--- routes/youthLeaderRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { createYouthLeader, getYouthLeaders } = require('../controllers/youthLeaderController');
-
-router.post('/', createYouthLeader);
-router.get('/', getYouthLeaders);
-
-module.exports = router;
